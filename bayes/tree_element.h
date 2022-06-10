e8f4fb6ea93cf56debe0fa6f97e8b72fb0c2333e
rabieja <aarabiej@gmail.com>
2022-06-10 15:40:38 +0200

    fix

--- bayes/tree_element.h
+++ bayes/tree_element.h
@@ -14,7 +14,7 @@ public:
 	virtual bool is_edge() = 0;
 	virtual bool is_root() = 0;
 	virtual void set_value() = 0;
-	virtual double get_value() = 0;
+	virtual long double get_value() = 0;
 	virtual void set_winner();
 	virtual void set_helper(bool helper_value) = 0;
 	virtual void find_winner() = 0;
